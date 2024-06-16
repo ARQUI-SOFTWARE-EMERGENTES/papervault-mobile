9b290ab4573e179acf9c1915b84e8e1a593920ea
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-06-16 17:24:52 -0500

    fix: add margin to avoid overflow

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -34,100 +34,103 @@ class SignUpPageState extends State<SignUpPage> {
       body: Center(
         child: SizedBox(
           width: MediaQuery.of(context).size.width * 0.9,      
-          child: Container(         
-            decoration: BoxDecoration(
-              color: Colors.lightBlue.shade200,
-              borderRadius: BorderRadius.circular(15)
-            ),
-            child: SingleChildScrollView(
-              child: Padding(              
-                padding: EdgeInsets.all(16.0),
-                child: Form(         
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Text(
-                          "Sign Up",
-                          style: GoogleFonts.arsenal(            
-                            color: Colors.black,
-                            fontSize: 45,
-                            fontWeight: FontWeight.w700     
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 20.0), 
+            child: Container(         
+              decoration: BoxDecoration(
+                color: Colors.lightBlue.shade200,
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: SingleChildScrollView(
+                child: Padding(              
+                  padding: EdgeInsets.all(16.0),
+                  child: Form(         
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text(
+                            "Sign Up",
+                            style: GoogleFonts.arsenal(            
+                              color: Colors.black,
+                              fontSize: 45,
+                              fontWeight: FontWeight.w700     
+                            )
                           )
-                        )
-                      ),
-                      const SizedBox(height: 25),
-                      CustomTextFormField(
-                        hintText: "Name",
-                        controller: nameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        }
-                      ),
-                      const SizedBox(height: 25),
-                      CustomTextFormField(
-                        hintText: "Last Name",
-                        controller: lastNameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        }
-                      ),
-                      const SizedBox(height: 25),
-                      CustomTextFormField(
-                        hintText: "Email",
-                        controller: emailController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        }
-                      ),
-                      const SizedBox(height: 20),
-                      CustomTextFormField(
-                        hintText: "Password",
-                        controller: passwordController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        }
-                      ),
-                      const SizedBox(height: 20),
-                      CustomTextFormField(
-                        hintText: "Repeat password",
-                        controller: passwordRepeatController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        }
-                      ),                      
-                      const SizedBox(height: 20),
-                      Center(                
-                        child: CustomCTAButton(text: "Sign Up", onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              // Process the login if form inputs are valid
-                              print("Username and Password validated!");
+                        ),
+                        const SizedBox(height: 25),
+                        CustomTextFormField(
+                          hintText: "Name",
+                          controller: nameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
                             }
+                            return null;
                           }
                         ),
-                      )
-                    ],
+                        const SizedBox(height: 25),
+                        CustomTextFormField(
+                          hintText: "Last Name",
+                          controller: lastNameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            return null;
+                          }
+                        ),
+                        const SizedBox(height: 25),
+                        CustomTextFormField(
+                          hintText: "Email",
+                          controller: emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            return null;
+                          }
+                        ),
+                        const SizedBox(height: 20),
+                        CustomTextFormField(
+                          hintText: "Password",
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          }
+                        ),
+                        const SizedBox(height: 20),
+                        CustomTextFormField(
+                          hintText: "Repeat password",
+                          controller: passwordRepeatController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          }
+                        ),                      
+                        const SizedBox(height: 20),
+                        Center(                
+                          child: CustomCTAButton(text: "Sign Up", onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                // Process the login if form inputs are valid
+                                print("Username and Password validated!");
+                              }
+                            }
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                )
               )
-            )
+            ),
           ),
         ),
       ),
