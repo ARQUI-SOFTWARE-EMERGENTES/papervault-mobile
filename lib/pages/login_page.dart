79120542df9a9037ff46b835958b4f8a88ca989b
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-06-16 22:40:52 -0500

    feat: add margin and padding

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -33,90 +33,95 @@ class LoginPageState extends State<LoginPage> {
         child: SizedBox(
           width: MediaQuery.of(context).size.width * 0.9, // Set container width as 90% of screen width
           height: MediaQuery.of(context).size.height * 0.6,      
-          child: Container(         
-            decoration: BoxDecoration(
-              color: Colors.lightBlue.shade200,
-              borderRadius: BorderRadius.circular(15)
-            ),
-            padding: const EdgeInsets.all(16.0),
-            child: Form(         
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Text(
-                      "Login",
-                      style: GoogleFonts.arsenal(            
-                        color: Colors.black,
-                        fontSize: 45,
-                        fontWeight: FontWeight.w700     
-                      )
-                    )        
-                  ),
-                  const SizedBox(height: 25),
-                  CustomTextFormField(
-                    hintText: "Email",
-                    controller: emailController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    }
-                  ),
-                  const SizedBox(height: 20),
-                  CustomTextFormField(
-                    hintText: "Password",
-                    controller: passwordController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    }
-                  ),
-                  const SizedBox(height: 20),
-                  Center(                
-                    child: CustomCTAButton(text: "Sign In", onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // Process the login if form inputs are valid
-                          print("Username and Password validated!");
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.lightBlue.shade200,
+                borderRadius: BorderRadius.circular(15)
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
+              child: SingleChildScrollView(
+                child: Form(         
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Login",
+                          style: GoogleFonts.arsenal(            
+                            color: Colors.black,
+                            fontSize: 45,
+                            fontWeight: FontWeight.w700     
+                          )
+                        )        
+                      ),
+                      const SizedBox(height: 25),
+                      CustomTextFormField(
+                        hintText: "Email",
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
                         }
-                        Navigator.pushNamed(context, ROUTE_SEARCH);
-                      }
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                    child: RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "Don't have an account? ",
-                            style: GoogleFonts.arsenal(            
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500
-                            )
-                          ),
-                          TextSpan(
-                            text: "Sign Up",
-                            style: GoogleFonts.arsenal(            
-                              color: Colors.blue.shade700,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              decoration: TextDecoration.underline
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = ()  => Navigator.pushNamed(context, ROUTE_SIGN_UP)
-                          ),
-                        ]
+                      ),
+                      const SizedBox(height: 20),
+                      CustomTextFormField(
+                        hintText: "Password",
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        }
+                      ),
+                      const SizedBox(height: 20),
+                      Center(                
+                        child: CustomCTAButton(text: "Sign In", onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Process the login if form inputs are valid
+                              print("Username and Password validated!");
+                            }
+                            Navigator.pushNamed(context, ROUTE_SEARCH);
+                          }
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Center(
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Don't have an account? ",
+                                style: GoogleFonts.arsenal(            
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500
+                                )
+                              ),
+                              TextSpan(
+                                text: "Sign Up",
+                                style: GoogleFonts.arsenal(            
+                                  color: Colors.blue.shade700,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  decoration: TextDecoration.underline
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = ()  => Navigator.pushNamed(context, ROUTE_SIGN_UP)
+                              ),
+                            ]
+                          )
+                        ),
                       )
-                    ),
-                  )
-                ],
+                    ],
+                  ),
+                ),
               ),
             ),
           )
